b269d2c69fbdc53e01aeb88ee59bfeaab3753034
hawkAlex <hawk.4el@gmail.com>
2015-11-11 23:17:34 +0300

    fix small moments

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 var App = App || {};
 
-App.Mine = function() {
+App.Mine = function () {
     var self = this;
 
     this.size = 0;
@@ -8,18 +8,17 @@ App.Mine = function() {
     this.mainElements = {};
     this.timerId = null;
 
-    this.render = function() {
+    this.render = function () {
         self.initHandlers();
 
     }
 
-    this.initHandlers = function() {
+    this.initHandlers = function () {
         var generateButton = document.getElementById('generete-button');
-        console.log(generateButton);
-        generateButton.addEventListener('click', function(){
+        generateButton.addEventListener('click', function () {
             self.openField = 1;
-            //cach DOM Element
 
+            //cach DOM Element
             self.mainElements.table = document.querySelector('#table');
             self.mainElements.genereteForm = document.querySelector('#generete-form');
             self.mainElements.countField = document.getElementById('mine-counter');
@@ -32,12 +31,10 @@ App.Mine = function() {
             self.calculeteNumberBesideMine(cells);
             self.mainElements.tbody.addEventListener('click', self.clickOnField);
             self.mainElements.tbody.addEventListener('contextmenu', self.demine);
-
-
         });
 
         var newGameButton = document.getElementById('new-game-button');
-        newGameButton.addEventListener('click', function(){
+        newGameButton.addEventListener('click', function () {
             self.mainElements.tbody.remove();
             self.mainElements.table.hidden = true;
             self.mainElements.genereteForm.hidden = false;
@@ -47,22 +44,22 @@ App.Mine = function() {
         });
     }
 
-    this.demine = function(e) {
+    this.demine = function (e) {
         e.preventDefault();
         var countField = self.mainElements.countField;
         var targetElement = e.target;
         if (targetElement.className != 'open-field') {
             if (targetElement.className != 'isMine') {
                 targetElement.classList.add('isMine');
-                countField.value = parseInt(countField.value) -1;
+                countField.value = parseInt(countField.value) - 1;
             } else {
                 targetElement.classList.remove('isMine');
-                countField.value = parseInt(countField.value) +1;
+                countField.value = parseInt(countField.value) + 1;
             }
         }
     }
 
-    this.clickOnField = function(e) {
+    this.clickOnField = function (e) {
         if (parseInt(document.getElementById('clock').value) == 0) {
             self.startTimer();
         }
@@ -73,23 +70,23 @@ App.Mine = function() {
                 targetElement.classList.add('show-mine');
                 self.showAllMineEndGame();
                 self.endGame();
-                setTimeout(function(){
-                    alert('Boooom...   Вы проиграли');
+                setTimeout(function () {
+                    alert('Boooom... Р’С‹ РїСЂРѕРёРіСЂР°Р»Рё!');
                 }, 100);
             } else {
                 if (targetElement.className != 'isMine') {
                     var bombCount = parseInt(targetElement.getAttribute('data-bomb-count'));
-                    if (bombCount != 0 ) {
+                    if (bombCount != 0) {
                         targetElement.innerText = bombCount;
                     }
                 }
 
                 if (targetElement.classList != 'open-field') {
-                    if (self.openField == (Math.pow(self.size, 2)  - self.mineCount)) {
+                    if (self.openField == (Math.pow(self.size, 2) - self.mineCount)) {
                         self.showAllMineEndGame();
                         self.endGame();
-                        setTimeout(function(){
-                            alert('Поздоавляем! Вы победили!!!');
+                        setTimeout(function () {
+                            alert('РџРѕР·РґСЂР°РІР»СЏРµРј! Р’С‹ РїРѕР±РµРґРёР»Рё!!!');
                         }, 100);
                     }
 
@@ -105,17 +102,17 @@ App.Mine = function() {
         }
     }
 
-    this.endGame = function() {
+    this.endGame = function () {
         self.mainElements.tbody.removeEventListener("contextmenu", self.demine);
         self.mainElements.tbody.removeEventListener('click', self.clickOnField);
         self.mainElements.clock.value = 0;
         self.stopTimer();
     }
 
-    this.generateField = function() {
+    this.generateField = function () {
         self.mainElements.genereteForm.hidden = true;
         self.mainElements.table.hidden = false;
-        var tbody  = document.createElement('tbody');
+        var tbody = document.createElement('tbody');
         tbody.setAttribute('id', 'tbody');
         self.mainElements.table.appendChild(tbody);
 
@@ -126,16 +123,15 @@ App.Mine = function() {
         this.mineCount = mineCount;
 
         var count = 0;
-        var mineArr =  this.generateMinePosition(size, mineCount);
+        var mineArr = this.generateMinePosition(size, mineCount);
 
         for (var i = 0; i < size; i++) {
             var tr = document.createElement('tr');
             for (var j = 0; j < size; j++) {
-                //console.log(tr);
                 var td = document.createElement('td');
                 td.setAttribute('x', i);
                 td.setAttribute('y', j);
-                td.setAttribute('coord', i+'-'+j)
+                td.setAttribute('coord', i + '-' + j)
 
                 if (mineArr.indexOf(count) != -1) {
                     td.setAttribute('data-mine', "1")
@@ -147,18 +143,18 @@ App.Mine = function() {
             }
         }
         var newGameButton = document.createElement('button');
-        newGameButton.innerText = 'Новая игра';
+        newGameButton.innerText = 'РќРѕРІР°СЏ РёРіСЂР°';
         var thead = table.querySelector('thead');
         thead.hidden = false;
         var mineCounterField = document.getElementById('mine-counter');
         mineCounterField.value = mineCount;
     }
 
-    this.generateMinePosition = function(size, mineCount) {
+    this.generateMinePosition = function (size, mineCount) {
         var mineArr = [];
-        var count = 0, maxValue = Math.pow(size,2);
+        var count = 0, maxValue = Math.pow(size, 2);
 
-        while ( count < mineCount ) {
+        while (count < mineCount) {
             var position = this.randomMinePosition(maxValue);
             if (mineArr.indexOf(position) == -1) {
                 mineArr.push(position);
@@ -168,13 +164,13 @@ App.Mine = function() {
         return mineArr;
     }
 
-    this.randomMinePosition = function(max) {
+    this.randomMinePosition = function (max) {
         var rand = Math.random() * (max + 1);
         rand = Math.floor(rand);
         return rand;
     }
 
-    this.calculeteNumberBesideMine = function(cells) {
+    this.calculeteNumberBesideMine = function (cells) {
         for (var i = 0; i < cells.length; i++) {
             if (!cells[i].getAttribute('data-mine')) {
                 var coord = cells[i].getAttribute('coord');
@@ -183,27 +179,27 @@ App.Mine = function() {
         }
     }
 
-    this.getMineCountForCell = function(coord, flag) {
+    this.getMineCountForCell = function (coord, flag) {
         var maxSize = this.size - 1;
         var coordXY = coord.split('-');
         x = parseInt(coordXY[0]);
         y = parseInt(coordXY[1]);
 
-        points = [[x-1, y-1],[x-1, y],[x-1, y+1],
-            [x, y-1],[x, y + 1],
-            [x+1, y-1],[x+1, y],[x+1, y+1]];
+        points = [[x - 1, y - 1], [x - 1, y], [x - 1, y + 1],
+            [x, y - 1], [x, y + 1],
+            [x + 1, y - 1], [x + 1, y], [x + 1, y + 1]];
         boombCount = 0;
 
         for (var j = 0; j < 8; j++) {
             if (points[j][0] >= 0 && points[j][1] >= 0 && points[j][0] <= maxSize && points[j][1] <= maxSize) {
                 var dataValue = points[j].join('-');
-                element =  document.querySelector('td[coord="' + dataValue + '"]');
+                element = document.querySelector('td[coord="' + dataValue + '"]');
 
-                if ( element.getAttribute('data-mine')) {
+                if (element.getAttribute('data-mine')) {
                     boombCount++;
                 }
 
-                if ( flag ) {
+                if (flag) {
                     if (parseInt(element.getAttribute('data-bomb-count')) > 0 && element.className != 'open-field') {
                         element.classList.add('open-field');
                         element.innerText = element.getAttribute('data-bomb-count');
@@ -216,18 +212,18 @@ App.Mine = function() {
         cell.setAttribute('data-bomb-count', boombCount);
     }
 
-    this.showAllMineEndGame = function(){
+    this.showAllMineEndGame = function () {
         var mineField = document.querySelectorAll('td[data-mine="1"]');
         for (var i = 0; i < mineField.length; i++) {
             mineField[i].classList.add('show-mine');
         }
     }
 
-    this.getAllEmptyField = function(coord) {
+    this.getAllEmptyField = function (coord) {
         this.openField++;
 
         var element = document.querySelector('td[coord="' + coord + '"]');
-        var coordXY  = coord.split('-');
+        var coordXY = coord.split('-');
         var x = parseInt(coordXY[0]);
         var y = parseInt(coordXY[1]);
         var maxSize = this.size - 1;
@@ -235,9 +231,9 @@ App.Mine = function() {
 
         this.getMineCountForCell(coord, true);
 
-        var  points = [[x-1, y],
-            [x, y-1],[x, y + 1],
-            [x+1, y]];
+        var points = [[x - 1, y],
+            [x, y - 1], [x, y + 1],
+            [x + 1, y]];
 
         for (var j = 0; j < 4; j++) {
             if (points[j][0] >= 0 && points[j][1] >= 0 && points[j][0] <= maxSize && points[j][1] <= maxSize) {
@@ -252,18 +248,18 @@ App.Mine = function() {
         }
     }
 
-    this.startTimer = function() {
-        self.timerId = setInterval(function(){
+    this.startTimer = function () {
+        self.timerId = setInterval(function () {
             self.mainElements.clock.value = parseInt(self.mainElements.clock.value) + 1
         }, 1000);
     }
 
-    this.stopTimer = function() {
-        console.log(self.timerId);
+    this.stopTimer = function () {
         clearInterval(self.timerId);
     }
 };
 
-(new App.Mine({
+window.onload = function () {
+    (new App.Mine({}).render());
+};
 
-}).render());
